5b7200bde3ba42b3f23ac6cc9a593b6e81bc7524
Aaron Bowen <aareano@users.noreply.github.com>
2016-04-21 03:59:14 -0400

    Update lock.ino

--- zinnobar/lock.ino
+++ zinnobar/lock.ino
@@ -1,4 +1,8 @@
 void check_combo() {
+  for (int i = 0; i < comboLength; i++) { 
+    Serial.print(userCombo[i]);
+    Serial.print(", ");
+  }
   for (int i = 0; i < comboLength; i++) {
     if (correctCombo[i] != userCombo[i]) {
       digitalWrite(alertRed, HIGH);
@@ -9,12 +13,15 @@ void check_combo() {
       delay(250);
       digitalWrite(alertRed, LOW);
       lockInputNum = 0; // reset the user input
+      Serial.print("Incorrect combination (at position ");
+      Serial.print(i);
+      Serial.println(")");
       return;
     }
   }
   // if we made it here, the combination is correct
   MasterSequenceNum++;
-  
+  Serial.println("Correct combination");
   // turn off the yellow led that was turned on in loop()
   digitalWrite(alertYellow, LOW);
 
