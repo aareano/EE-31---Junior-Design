dc2e911b5d221598726b49a3c782cde6621622cc
Aaron Bowen <aareano@users.noreply.github.com>
2016-04-23 22:16:33 -0400

    Update optics.ino

--- zinnobar/optics.ino
+++ zinnobar/optics.ino
@@ -3,18 +3,26 @@
 
 void detect_color() {
   // illuminate blue LED
-  digitalWrite(BLUE_LED, HIGH);
+  if (PathToFollow == RED) {
+    digitalWrite(RED_LED, HIGH);
+  } else {
+    digitalWrite(BLUE_LED, HIGH);
+  }
   delay(1);
 
   // check the sensor
   float reading = analogRead(LED_SENSOR_LEVEL);
-  
-  // turn off blue LED
-  digitalWrite(BLUE_LED, LOW);
 
-   Serial.print("reading: ");
-   Serial.println(calcVolts(reading));
-  
+  // turn off blue LED
+  if (PathToFollow == RED) {
+    digitalWrite(RED_LED, LOW);
+  } else {
+    digitalWrite(BLUE_LED, LOW);
+  }
+
+  Serial.print("reading: ");
+  Serial.println(calcVolts(reading));
+
   if (calcVolts(reading) < BLUE_THRESHOLD) {
     ColorState = BLUE;
     Serial.println("BLUE");
@@ -39,64 +47,42 @@ void follow_path() {
 
   switch (PathState) {
     case PATH_FINDING:
-      // Serial.println("path finding...");
       if (ColorState == PathToFollow) {
         PathState = ON_PATH;
       }
-      forward_slow();
+      forward();
       break;
     case ON_PATH:
-      // Serial.println("on path!");
-      if (ColorState != PathToFollow) { // we've just moved off the path
-        PathState = SEARCHING_FIRST_SIDE;
-        SearchStartTime = millis();
-      }
-      SearchTime = BaseSearchTime;  // reset the search radius
-      forward_slow();
-      break;
-    case SEARCHING_FIRST_SIDE:
-      // Serial.print("searching first side - ");
-      // Serial.println(SearchSide == SEARCHING_LEFT ? "SEARCHING_LEFT" : "SEARCHING_RIGHT");
-
-      if ((millis() - SearchStartTime > MinSearchTime) && ColorState == PathToFollow) {
-        // Serial.println("checking for path!!!!!!!!!!!");
-        PathState = ON_PATH; // check at the end search time
-      }
-      
-      if (millis() - SearchStartTime > SearchTime) { // search time expired
-        
-        PathState = SEARCHING_SECOND_SIDE;
-        SearchSide = SearchSide == SEARCHING_LEFT ? SEARCHING_RIGHT : SEARCHING_LEFT; // flip the search side
-        SearchStartTime = millis();
-      } else {
-        if (SearchSide == SEARCHING_LEFT) {
-          turnLeftInPlace();
+      if (ColorState != PathToFollow) {
+        //         we've just moved off the path
+        PathState = RECOVERING;
+        if (PathToFollow == RED) {
+          SearchSide = SEARCHING_RIGHT;
         } else {
-          turnRightInPlace();
+          SearchSide = SEARCHING_LEFT;
         }
-      }
-      break;
-    case SEARCHING_SECOND_SIDE:
-      // Serial.print("searching second side - ");
-      // Serial.println(SearchSide == SEARCHING_LEFT ? "SEARCHING_LEFT" : "SEARCHING_RIGHT");
-
-      if ((millis() - SearchStartTime > MinSearchTime) && ColorState == PathToFollow) {
-        PathState = ON_PATH; // check at the end search time
-      }
-      
-      if (millis() - SearchStartTime > (2 * SearchTime)) { // search time expired
-
-        SearchTime *= 1.5;  // increase the radius by a factor of 1.5
-        PathState = SEARCHING_FIRST_SIDE;
-        SearchSide = SearchSide == SEARCHING_LEFT ? SEARCHING_RIGHT : SEARCHING_LEFT; // flip the search side
         SearchStartTime = millis();
+        SearchTime = BaseSearchTime;  // reset the search radius
       } else {
-        if (SearchSide == SEARCHING_LEFT) {
-          turnLeftInPlace();
-        } else {
-          turnRightInPlace();
-        }
+        forward();
       }
       break;
+    case RECOVERING: {
+        if ((millis() > SearchStartTime + MinSearchTime) && ColorState == PathToFollow) {
+          PathState = ON_PATH;
+          forward();
+          return;
+        } else if (millis() > SearchStartTime + SearchTime) { // search time expired
+          SearchTime *= 2;
+          SearchSide = SearchSide == SEARCHING_LEFT ? SEARCHING_RIGHT : SEARCHING_LEFT; // flip the search side
+          SearchStartTime = millis();
+        } else {
+          if (SearchSide == SEARCHING_LEFT) {
+            turnLeftInPlace();
+          } else {
+            turnRightInPlace();
+          }
+        }
+      } break;
   }
 }
