fbba49e7e70c94e22fc3ed9f4717acf40f34133f
Aaron Bowen <aareano@users.noreply.github.com>
2016-04-23 22:15:25 -0400

    Update zinnobar.ino

--- zinnobar/zinnobar.ino
+++ zinnobar/zinnobar.ino
@@ -27,30 +27,30 @@ enum MotorName { LEFT, RIGHT };
 
 int time360 = 1475; // ms
 int time180 = 750; // ms
-int forwardSpeed = 32; // ms/s
 
 // optic sensor
+int RED_LED = 50;
 int BLUE_LED = 51;
 
 int LED_SENSOR_LEVEL = A1;
 
 // Maximum voltage readings for that color (with the appropriate LED lit)
 float BLUE_THRESHOLD = 4.6;
-float RED_THRESHOLD = 4.9;
+float RED_THRESHOLD = 4.83;
 
 // represents the color the bot has most recently detected
 enum Color { BLACK, BLUE, RED };
 Color ColorState;
 
 // Line Following
-enum Path { PATH_FINDING, SEARCHING_FIRST_SIDE, SEARCHING_SECOND_SIDE, ON_PATH };
+enum Path { PATH_FINDING, ON_PATH, RECOVERING };
 enum Side { SEARCHING_LEFT, SEARCHING_RIGHT };
 Color PathToFollow;
 Side SearchSide;
 Path PathState;
-long BaseSearchTime = 200;
-long SearchTime = 200;      // this is how long to search for the line on the left or right when we get off of it
-long MinSearchTime = 30;   // the bot needs to turn at least this much before it starts checking for the path again
+long BaseSearchTime = 500;  // this value is later lowered to 100
+long SearchTime = 500;      // this is how long to search for the line on the left or right when we get off of it
+long MinSearchTime = 50;    // the bot needs to turn at least this much before it starts checking for the path again
 long SearchStartTime = 0;   // this will be edited when we start searching for the line when we get off of it
 
 enum TurnDirection {L, R};
@@ -129,7 +129,7 @@ int commsAlert = 37;
 int alertRed = 32;
 int alertYellow = 28;
 int alertBlue = 30;
-enum Message { BEGIN, FOUND_MINE, FINISHED, COMPANION_MOVE };
+enum Message { FOUND_MINE_SCARLET_WITCH, PATH_END, FOUND_MINE_NIGHTWING, DANCE};
 enum Comms { COMMS_LISTENING, COMMS_RECEIVING };
 Comms CommsState;
 
@@ -142,49 +142,86 @@ int lockInputNum = 0;
 
 // Master State
 enum Master { 
-  LOCKED, 
+  // challenge 1
+  LOCKED,           // LOCKED is challenge 3
   SETTINGS, 
   APPLY_SETTINGS, 
   LISTENING_MY_TURN, 
-  LISTENING_COMPANIONS_TURN, 
   FIND_WALL,
   DISCOVER_PATH,  
   FOLLOW_PATH_1, 
-  LISTENING_MINE, 
+  LISTENING_MINE_SCARLET_WITCH,
+  LISTENING_MINE_NIGHTWING, 
   FOLLOW_PATH_2, 
   FINAL_COLLISION, 
   FINAL_WAIT, 
   END,
+  QUIET,
+  
+  // challenge 2
+  HALT,
+  FORWARD_12,
+  ROTATE_RIGHT_180,   // arbitrary, could be LEFT
+  BACK_3,
+  TURN_LEFT,
+  TURN_RIGHT_1,
+  TURN_RIGHT_2,
+  TURN_RIGHT_3,
+  
+  // test
   TEST_TRANSMITTER,
   TEST_RECEIVER,
-  TEST_ROTATION_TIME };
+  TEST_ROTATION_TIME,
+  TEST_COLOR_DETECTION,
+  TEST_HALL_EFFECT,
+  TEST_PATH_FINDING
+};
 Master InitializeSequence[] = { LOCKED, SETTINGS, APPLY_SETTINGS };
-Master ScarletWitchSequence[] = { 
-  LISTENING_MY_TURN, // listen for 200ms from command center
+Master ScarletWitchSequence[] = {
   FIND_WALL,        // find wall (collision)
   DISCOVER_PATH,    // back up from wall until on top of path
   FOLLOW_PATH_1,    // follow path to find mine
-  LISTENING_MINE,   // wait for 300ms response from command center
+  LISTENING_MINE_SCARLET_WITCH,   // wait for 300ms response from command center
   FOLLOW_PATH_2,    // follow path to find end 
   FINAL_COLLISION,  // blinks led
-  FINAL_WAIT,       // waits for 400 hz from other bot
-  END };            // blinks leds 10 times
-Master NightwingSequence[] = { 
-  LISTENING_COMPANIONS_TURN,
-  LISTENING_MY_TURN,
+  FINAL_WAIT,       // waits for 300 ms from other bot
+  END,
+  QUIET };            // blinks leds 10 times
+Master NightwingSequence[] = {
+  LISTENING_MY_TURN,// wait for 300ms pulse
   FIND_WALL,        // find wall (collision)
   DISCOVER_PATH,    // back up from wall until on top of path
   FOLLOW_PATH_1,    // follow path to find mine
-  LISTENING_MINE,   // wait for 300ms response from command center
+  LISTENING_MINE_NIGHTWING,   // wait for 300ms response from command center
   FOLLOW_PATH_2,    // follow path to find end 
-  FINAL_COLLISION,  // blinks led
-  END };
-Master TestBotSequence[] = { TEST_ROTATION_TIME, TEST_TRANSMITTER, TEST_RECEIVER };
+  FINAL_COLLISION,  // sends 300 ms, blinks led
+  END,
+  QUIET };
+Master DanceSequence[] = {
+  HALT,
+  FORWARD_12,
+  HALT,
+  ROTATE_RIGHT_180, // arbitrary, could be LEFT
+  HALT,
+  BACK_3,
+  HALT,
+  TURN_LEFT,
+  TURN_RIGHT_1,
+  TURN_RIGHT_2,
+  TURN_RIGHT_3, 
+  // we are free to add more actions
+  QUIET
+};
+Master TestBotSequence[] = { TEST_HALL_EFFECT, TEST_COLOR_DETECTION, TEST_ROTATION_TIME, TEST_TRANSMITTER, TEST_RECEIVER };
 Master *MasterSequence = InitializeSequence; 
 int MasterSequenceNum = 0;
 
-enum Bot { NIGHTWING, SCARLET_WITCH, TEST_BOT };  // NIGHTWING goes first, then SCARLET_WITCH
+enum Bot { SCARLET_WITCH, NIGHTWING, DANCER, TEST_BOT };  // NIGHTWING goes first, then SCARLET_WITCH
 Bot BotType;
+
+// dance routine
+long DanceStepStartTime = 0;
+boolean NotifyFinishedDanceStep = true;
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -195,8 +232,8 @@ void setup() {
   pinMode(GATE3, OUTPUT);
   pinMode(GATE4, OUTPUT);
 
+  pinMode(RED_LED, OUTPUT);
   pinMode(BLUE_LED, OUTPUT);
-  
   pinMode(LED_SENSOR_LEVEL, INPUT);
 
   pinMode(H_SENSOR, INPUT);
@@ -266,36 +303,33 @@ void loop() {
           MasterSequence = ScarletWitchSequence;
           MasterSequenceNum = 0;
           PathToFollow = RED;
-          flash_led(alertRed, 250);
+          flash_led(alertRed, 100);
           break;
         case NIGHTWING:
-          MasterSequence = ScarletWitchSequence; // NightwingSequence;
+          MasterSequence = NightwingSequence;
           MasterSequenceNum = 0;
           PathToFollow = BLUE;
-          flash_led(alertBlue, 250);
+          flash_led(alertBlue, 100);
+          break;
+        case DANCER:
+          MasterSequence = DanceSequence;
+          MasterSequenceNum = 0;
+          flash_led(alertYellow, 100);
           break;
         case TEST_BOT:
           MasterSequence = TestBotSequence;
           MasterSequenceNum = 0;
           PathToFollow = RED;
-          flash_led(alertYellow, 250);
+          flash_led(alertRed, 50);
+          flash_led(alertYellow, 50);
+          flash_led(alertBlue, 50);
           break;
         default:
           ; // do nothing
       }
       break;
-    case LISTENING_MY_TURN:         // listen for 200ms from command center
+    case LISTENING_MY_TURN:         // listen for 300 ms from command center
       Serial.println("LISTENING_MY_TURN");
-      halt();
-      drive();
-      if (CommsState == COMMS_LISTENING) {
-        poll_comms();
-      }
-      if (CommsState == COMMS_RECEIVING) {
-        receive_message();
-      }
-      break;
-    case LISTENING_COMPANIONS_TURN: // listen for second 200ms from command center
       halt();
       drive();
       if (CommsState == COMMS_LISTENING) {
@@ -319,6 +353,8 @@ void loop() {
       if (ColorState == PathToFollow) {
         MasterSequenceNum++; // found the path!
       }
+      poll_bumpers();
+      service_collisions();
       drive();      
       break;
     case FOLLOW_PATH_1:             // follow path to find mine
@@ -338,7 +374,17 @@ void loop() {
       service_h_sensor();
       drive();
       break;
-    case LISTENING_MINE:            // wait for 300ms response from command center
+    case LISTENING_MINE_SCARLET_WITCH:            // wait for 200 ms response from command center
+      halt();
+      drive();
+      if (CommsState == COMMS_LISTENING) {
+        poll_comms();
+      }
+      if (CommsState == COMMS_RECEIVING) {
+        receive_message();
+      }
+      break;
+    case LISTENING_MINE_NIGHTWING:              // wait for 400 ms response from command center
       halt();
       drive();
       if (CommsState == COMMS_LISTENING) {
@@ -357,8 +403,8 @@ void loop() {
       drive();
       break;
     case FINAL_COLLISION:           // blinks led
-      send_message(FINISHED);
-      send_message(FINISHED);
+      send_message(PATH_END);
+      send_message(PATH_END);
       MasterSequenceNum++;
       break;
     case FINAL_WAIT:                // waits for 400 hz from other bot
@@ -371,8 +417,10 @@ void loop() {
         receive_message();
       }
       break;
-    case END:                       // blinks leds 10 times
-      for (int i = 0; i < 3; i++) {
+    case END: {                       // blinks leds 10 times
+      halt();
+      drive();
+      for (int i = 0; i < 10; i++) {
         digitalWrite(alertRed, HIGH);
         digitalWrite(alertYellow, HIGH);
         digitalWrite(alertBlue, HIGH);
@@ -380,20 +428,138 @@ void loop() {
         digitalWrite(alertRed, LOW);
         digitalWrite(alertYellow, LOW);
         digitalWrite(alertBlue, LOW);
+        delay(200);
       }
+    } break;
+    case QUIET: {
+      digitalWrite(alertRed, LOW);
+      digitalWrite(alertYellow, LOW);
+      digitalWrite(alertBlue, LOW);
       halt();
       drive();
-      delay(1000);
-      break;
+    } break;
+                                              // challenge 2 --------------------------------
+    case HALT: {
+      Serial.println("HALT");             
+      halt();
+      drive();
+      if (NotifyFinishedDanceStep) {
+        notify_finished();
+      }
+      listen_for_message();
+    } break;
+    case FORWARD_12: {
+      Serial.println("FORWARD_12");
+      if (millis() < DanceStepStartTime + get_drive_time(12)) {
+        forward();
+        drive();
+      } else {
+        halt();
+        drive();
+        if (NotifyFinishedDanceStep) {
+          notify_finished();
+        }
+      }
+      listen_for_message();
+    } break;
+    case ROTATE_RIGHT_180: {
+      Serial.println("ROTATE_RIGHT_180");
+      if (millis() < DanceStepStartTime + get_rotate_time(180)) {
+        turnRightInPlace();
+      } else {
+        halt();
+        drive();
+        if (NotifyFinishedDanceStep) {
+          notify_finished();
+        }
+      }
+      listen_for_message();
+      drive();
+    } break;
+    case BACK_3: {
+      Serial.println("BACK_3");
+      if (millis() < DanceStepStartTime + get_drive_time(3)) {
+        reverse();
+      } else {
+        halt();
+        drive();
+        if (NotifyFinishedDanceStep) {
+          notify_finished();
+        }
+      }
+      listen_for_message();
+      drive();
+    } break;
+    case TURN_LEFT: {
+      Serial.println("TURN_LEFT");
+      long danceStepTime = 2000; // ms
+      if (millis() < DanceStepStartTime + danceStepTime) {
+        turnLeft();
+      } else {
+        halt();
+        drive();
+        if (NotifyFinishedDanceStep) {
+          notify_finished();
+        }
+      }
+      listen_for_message();
+      drive();
+    } break;
+    case TURN_RIGHT_1: {
+      Serial.println("TURN_RIGHT_1");
+      long danceStepTime = 1000; // ms
+      if (millis() < DanceStepStartTime + danceStepTime) {
+        turnRight();
+      } else {
+        halt();
+        drive();
+        if (NotifyFinishedDanceStep) {
+          notify_finished();
+        }
+      }
+      listen_for_message();
+      drive();
+    } break;
+    case TURN_RIGHT_2: {
+      Serial.println("TURN_RIGHT_2");
+      long danceStepTime = 1000; // ms
+      if (millis() < DanceStepStartTime + danceStepTime) {
+        turnRight();
+      } else {
+        halt();
+        drive();
+        if (NotifyFinishedDanceStep) {
+          notify_finished();
+        }
+      }
+      listen_for_message();
+      drive();
+    } break;
+    case TURN_RIGHT_3: {
+      Serial.println("TURN_RIGHT_3");
+      long danceStepTime = 1000; // ms
+      if (millis() < DanceStepStartTime + danceStepTime) {
+        turnRight();
+      } else {
+        halt();
+        drive();
+        if (NotifyFinishedDanceStep) {
+          notify_finished();
+        }
+      }
+      listen_for_message();
+      drive();
+    } break;
+                                                  // test --------------------------------
     case TEST_TRANSMITTER:          // send each message, indicated with LEDs
       halt();
       drive();
       poll_bumpers();
       service_collisions();
       
-      send_message(BEGIN);
-      send_message(FOUND_MINE);
-      send_message(FINISHED);
+      send_message(FOUND_MINE_SCARLET_WITCH);
+      send_message(PATH_END);
+      send_message(FOUND_MINE_NIGHTWING);
       break;
     case TEST_RECEIVER:             // recieve messages, indicated with LEDs
       halt();
@@ -408,30 +574,55 @@ void loop() {
       }
       break;
     case TEST_ROTATION_TIME:
-//      turnRightInPlace();
-//      drive();
-//      delay(1475);
-//      halt();
-//      drive();
-//      delay(1000);
-//      turnLeftInPlace();
-//      drive();
-//      delay(750);
-//      halt();
-//      drive();
-//      delay(1000);
-      forward();
+      turnRightInPlace();
       drive();
-      delay(2000);
+      delay(1475);
       halt();
       drive();
-      delay(2000);
-      reverse();
+      delay(1000);
+      turnLeftInPlace();
       drive();
-      delay(2000);
+      delay(750);
       halt();
       drive();
-      delay(2000);
+      delay(10000);
+      break;
+    case TEST_COLOR_DETECTION:
+      Serial.println("TEST_COLOR_DETECTION");
+      detect_color();
+      if (ColorState == RED)
+        light_led(alertRed);
+      else if (ColorState == BLUE)
+        light_led(alertBlue);
+      else
+        light_led(alertYellow);
+      delay(700);
+      break;
+    case TEST_HALL_EFFECT:
+      Serial.println("TEST_COLOR_DETECTION");
+      halt();
+      poll_h_sensor();
+      service_h_sensor_test();
+      drive();
+    case TEST_PATH_FINDING:
+      detect_color();
+      follow_path();
+      break;
+  }
+}
+
+void notify_finished() {
+  send_message(DANCE); // 400 ms
+  send_message(DANCE);
+  NotifyFinishedDanceStep = false;
+}
+
+void listen_for_message() {
+  if (CommsState == COMMS_LISTENING) {
+    poll_comms();
+  }
+  if (CommsState == COMMS_RECEIVING) {
+    receive_message();
   }
 }
 
